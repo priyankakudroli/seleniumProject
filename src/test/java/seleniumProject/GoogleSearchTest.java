004fc7d13f8b9873479f7c882e65fefd202cb88f
priyankakudroli <priyanka.mk@tcs.com>
2023-10-26 06:55:14 +0000

    Updated GoogleSearchTest for findElements

--- src/test/java/seleniumProject/GoogleSearchTest.java
+++ src/test/java/seleniumProject/GoogleSearchTest.java
@@ -1,20 +1,35 @@
 package seleniumProject;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSearchTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		
-		driver.findElement(By.id("APjFqb")).sendKeys("selenium");
+		driver.findElement(By.id("APjFqb")).sendKeys("selenium tutorial");
 		driver.getTitle();
-
+		Thread.sleep(2000);
+		List<WebElement> items = driver.findElements(By.xpath("(//ul[@class='G43f7e'])[1]//li//descendant::div[@class='wM6W7d']"));
+		System.out.println(items.size());
+		
+		for (WebElement item : items) 
+		{
+			System.out.println(item.getText());
+			if(item.getText().equalsIgnoreCase("Selenium tutorial pdf"))
+			{
+				item.click();
+				break;
+			}
+		}
 	}
 
 }
