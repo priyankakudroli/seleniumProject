ad06f56a3471b07bea5ea930b87d16a9cef47fff
priyankakudroli <priyanka.mk@tcs.com>
2023-10-27 06:55:33 +0000

    Updates to Check box and Radio Button Test

--- src/test/java/seleniumProject/CheckBoxAndRadioButtonTest.java
+++ src/test/java/seleniumProject/CheckBoxAndRadioButtonTest.java
@@ -2,6 +2,7 @@ package seleniumProject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CheckBoxAndRadioButtonTest {
@@ -10,7 +11,7 @@ public class CheckBoxAndRadioButtonTest {
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://demo.seleniumeasy.com/basic-checkbox-demo.html");
-		
+		//CheckBox Test
 		boolean isAgeClicked = driver.findElement(By.id("isAgeSelected")).isSelected();
 		
 		if(!isAgeClicked)
@@ -18,6 +19,16 @@ public class CheckBoxAndRadioButtonTest {
 			driver.findElement(By.id("isAgeSelected")).click();
 		}
 		
+		//RadioButtonTest
+		
+		driver.get("http://demo.seleniumeasy.com/basic-radiobutton-demo.html");
+		WebElement ageGroup = driver.findElement(By.xpath("//input[@value ='15 - 50']"));
+		
+		if(!ageGroup.isSelected())
+		{
+			ageGroup.click();
+			System.out.println("The 5-15 age group option is selected");
+		}
 		driver.close();
 	}
 
